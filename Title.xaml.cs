7b9645a7c09defb9b06e7f19b2970a9fd29dc6ed
rpg1005 <42277864+rpg1005@users.noreply.github.com>
2020-02-13 08:31:16 -0500

    Add files via upload

---
+++ Title.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rockfoodrescue
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Title : ContentPage
+    {
+        public Title()
+        {
+            InitializeComponent();
+        }
+        private async void Enter_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoList());
+        }
+    }
+}
