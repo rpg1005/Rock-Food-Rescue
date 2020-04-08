27fa6fd3ed088dc4d381cc5d5288117b3ce03381
rpg1005 <42277864+rpg1005@users.noreply.github.com>
2020-04-08 11:05:17 -0400

    Add files via upload

---
+++ adminTitle.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace foodadmin
+{
+    public partial class Title : ContentPage
+    {
+        TodoItemManager manager;
+        public Title()
+        {
+            InitializeComponent();
+            manager = TodoItemManager.DefaultManager;
+        }
+        private async void Enter_Clicked(object sender, EventArgs e)
+        {
+            ObservableCollection<TodoItem> userList = await manager.GetLoginAsync();
+            foreach (TodoItem item in userList)
+            {
+                if (username.Text == item.UserName && password.Text == item.PassWord)
+                {
+                    await Navigation.PushAsync(new TabbedPage1());
+                }
+                else
+                {
+                    await DisplayAlert("Alert", "Incorrect Username or Password", "OK");
+                }
+            }
+        }
+    }
+}
